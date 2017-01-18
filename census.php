13eeb0cb653d4866a448c9801785afd94d284d13
carlomnop <cuevascarlo07>
2017-01-18 16:59:29 +0800

    change census form added navigation bar

--- census.php
+++ census.php
@@ -1,3 +1,63 @@
 <?php //FRONT END
   //page kung san sila mag fifill up ng census
+  $thisPage = "ApplicationForm";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/census.css">
+  </head>
+  <body>
+    <?php include_once('nav.php');?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <label>Question 1</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 2</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 3</label>
+          <input type="text" class="form-control"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <label>Question 1</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 2</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 3</label>
+          <input type="text" class="form-control"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <label>Question 1</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 2</label>
+          <input type="text" class="form-control"/>
+        </div>
+        <div class="col-md-4">
+          <label>Question 3</label>
+          <input type="text" class="form-control"/>
+        </div>
+      </div>
+    </div>
+  </body>
+  <script type="text/javascript" src="bootstrap/js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="bootstrap/js/bootstrap-notify.min.js"></script>
+  <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</html>
