187ffb47a2bd5745e540fb8909150eec7ffcb412
carlomnop <cuevascarlo07@gmail.com>
2017-01-21 10:13:20 +0800

    modified nav.php added other buttons in index to have a homepage

--- nav.php
+++ nav.php
@@ -1,22 +1,32 @@
 <?php //FRONT END
   //centralized nav, dito rin ung search para kahit san pde mag search agad ng census form
-  $thisPage;
+
+  /*<li class="<?php if($thisPage=='ApplicationForm'){echo 'active'; }?>"><a href="census.php">Application Form</a></li>*/
 ?>
-<nav class="navbar navbar-inverse" >
+<nav class="navbar navbar-default navbar-fixed-top  " >
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Online Census</a>
+      <p class="navbar-text" style="font-weight:bold;">Online Census</p>
     </div>
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav" id="myNavbar">
       <li class="<?php if($thisPage=='Home'){echo 'active'; }?>"><a href="index.php">Home</a></li>
-      <li class="<?php if($thisPage=='ApplicationForm'){echo 'active'; }?>"><a href="census.php">Application Form</a></li>
+      <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section2">Contacts</a></li>
+      <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section3">Other Information</a></li>
+      <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section41">E-Census</a></li>
+      <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section42">Footer</a></li>
+      <li class="<?php if($thisPage=='ApplicationForm'){echo 'active'; }?>" <?php if($thisPage=='Home'){echo 'style="display:none;"';}?>><a href="census.php">Application Form</a></li>
+
       <li><a href="#"></a></li>
     </ul>
     <form class="navbar-form navbar-right">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Put Application Number...">
-      </div>
-      <button type="submit" class="btn btn-default">Search Application Form</button>
+      <div class="input-group">
+         <input type="text" class="form-control" placeholder="Search">
+         <div class="input-group-btn">
+           <button class="btn btn-default" type="submit">
+             <i class="glyphicon glyphicon-search"></i>
+           </button>
+         </div>
+       </div>
     </form>
   </div>
 </nav>
