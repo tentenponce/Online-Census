af2988c589a15c2443addea3656290dfe2bfdce5
carlomnop <cuevascarlo07@gmail.com>
2017-01-22 23:42:18 +0800

    updated functions in nav.php

--- nav.php
+++ nav.php
@@ -3,13 +3,13 @@
 
   /*<li class="<?php if($thisPage=='ApplicationForm'){echo 'active'; }?>"><a href="census.php">Application Form</a></li>*/
 ?>
-<nav class="navbar navbar-default navbar-fixed-top  " >
+<nav class="navbar navbar-inverse navbar-fixed-top  " >
   <div class="container-fluid">
     <div class="navbar-header">
       <p class="navbar-text" style="font-weight:bold;">Online Census</p>
     </div>
     <ul class="nav navbar-nav" id="myNavbar">
-      <li class="<?php if($thisPage=='Home'){echo 'active'; }?>"><a href="index.php">Home</a></li>
+      <li class="<?php if($thisPage=='Home'){echo 'active'; }?>"><a href="<?php if($thisPage=='Home'){echo '#section1';} elseif($thisPage=='ApplicationForm'){echo 'index.php';}?>">Home</a></li>
       <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section2">Contacts</a></li>
       <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section3">Other Information</a></li>
       <li <?php if($thisPage=='ApplicationForm'){echo 'style="display:none;"';}?>><a href="#section41">E-Census</a></li>
