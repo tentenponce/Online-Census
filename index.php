c00691fde6704cf04baef95cb886df4f74d5d7d6
carlomnop <cuevascarlo07@gmail.com>
2017-01-25 01:10:50 +0800

    updated index with some animations

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <html lang="en">
   <head>
     <title>Online-Census</title>
+    <link rel="icon" href="images/logo.png" type="image/png" sizes="16x22">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
     <link rel="stylesheet" type="text/css" href="css/census.css">
     <script type="text/javascript" src="bootstrap/js/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
@@ -16,17 +18,17 @@
   </head>
   <body>
     <?php include_once('nav.php');?>
-      <div id="section1" class="container-fluid">
+      <div id="section1" class="wow container-fluid">
         <br/>
         <br/>
         <br/>
-          <h1 class="header">E-Census</h1>
-          <p class="meaning">Carousel + important images here</p>
+          <h1 class="header wow bounceInUp">E-Census</h1>
+          <p class="header-message wow fadeInRight">Online Census meaning</p>
       </div>
       <div id="section2" class="container-fluid">
         <div class="message2">
-          <h1>Importance</h1>
-          <span>Importance of e-Census</span>
+          <h1 class="header wow bounceInUp"  data-wow-offset="10">Announcements</h1>
+          <span class="wow slideInRight">aa</span>
         </div>
       </div>
       <div id="section3" class="container-fluid">
@@ -39,7 +41,7 @@
         <div class="message4">
           <h1>ApplicationForm</h1>
           <p>Application form</p>
-          <a href="census.php"><button class = "btn primary">Application Form</button></a>
+          <a href="census1.php"><button class = "btn primary">Application Form</button></a>
         </div>
       </div>
       <div id="section42" class="container-fluid">
@@ -51,25 +53,7 @@
     </body>
   <script type="text/javascript" src="bootstrap/js/bootstrap-notify.min.js"></script>
   <script>
-  $(window).on('scroll', function() {
-    var y_scroll_pos = window.pageYOffset; // set to whatever you want it to be
-
-
-      if(y_scroll_pos > 600) {
-
-          $(".message2").animate({left: '250px'});
-          $(".message2").fadeIn();
-      }
-      if(y_scroll_pos > 1200) {
-          $(".message3").animate({marginLeft : "100px"},"slow","swing");
-      }
-      if(y_scroll_pos > 1800) {
-          $(".message4").fadeIn("slow");
-      }
-
-  });
   $(document).ready(function(){
-    $(".header").animate({marginLeft : "25px"},"slow","swing");
     // Add scrollspy to <body>
     $('body').scrollspy({target: ".navbar", offset: 50});
     // Add smooth scrolling on all links inside the navbar
@@ -94,5 +78,9 @@
         }  // End if
       });
     });
+  </script>
+  <script type="text/javascript" src="js/wow.min.js"></script>
+  <script>
+    new WOW().init();
   </script>
 </html>
