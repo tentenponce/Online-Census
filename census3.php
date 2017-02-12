b5269e48200a29f63d070241e411741f28f9a272
Tenten Ponce <poncetenten10@gmail.com>
2017-02-12 14:12:36 +0800

    Added Initial Form 4

--- census3.php
+++ census3.php
@@ -19,6 +19,27 @@
     <?php include_once('nav.php');?>
     <form method='post' action='ajax/addMember.php' class='container' style="margin-top:75px;">
       <button type='button' onclick='nextPage()' class="btn btn-primary" style="outline-style:none;width:60px;font-size:24px;height:60px;border-radius:100%;bottom:0; right:0;margin:50px; margin-right:50px; margin-top:20px; position:fixed;"><span class="glyphicon glyphicon-menu-right"></span></button>
+
+      <h4 class='text-center'><b>Form 2D</b></h4>
+      <hr />
+      <div class='row'>
+        <!--house_type-->
+        <div class='col-md-3'>
+          <p class='title'>Type of building/house</p>
+
+          <div class="form-group">
+            <select class='form-control' name='house_type' required>
+              <option value='1'>Single House</option>
+              <option value='2'>Duplex</option>
+              <option value='3'>Multi-unit residential (three units or more)</option>
+              <option value='4'>Commercial/industrial/agricultural (office, factory, and others)</option>
+              <option value='5'>Institutional living quarter (hotel, hospital, and others)</option>
+              <option value='6'>Other housing units (boat, cave, and others)</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
     </form>
   </body>
   <script type="text/javascript" src="bootstrap/js/jquery-3.1.1.min.js"></script>
